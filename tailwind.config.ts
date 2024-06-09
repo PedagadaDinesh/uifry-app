30367b1cb9ace33abecdf3670938deeb03aedb98
peddagadadinesh2000@gmail.com <peddagadadinesh2000@gmail.com>
2024-06-09 23:45:01 +0530

    Changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,42 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#FF5555",
+        // secondary: "#0EA861",
+        facebook: "#4267B2",
+        instagram: "#bc2a8d",
+        twitter: "#1da1f2",
+        linkedin: "#0077b5",
+        pinterest: "#E60023",
+        whatsapp: "#25d366",
+        youtube: "#cd201f",
+        light: "#EFF6FF",
+        dark: "#0e0e23",
+        // "pink-blue": "#6259ca",
+      },
+      fontSize: {
+        small: "15px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow: {
+        "shadow-primary": "[0_0.25rem_1.125rem_rgba(75,70,92,0.1)]",
+      },
+      textColor: {
+        "primary-text": "#5d596c",
+      },
     },
   },
   plugins: [],
 };
-export default config;
